c403df8911b5d5d236a2be4cc09d2bc930acf89c
sinharzs2k26 <sinharzs2k26@gmail.com>
2026-01-17 08:44:35 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "8221663079:AAHR01BGwId6N4aL8HYu7yUwBK4n6eJ8Fng"
+TOKEN = "7786701907:AAEj2FwZnCfhYUQzm2BF65-IPmRaGsJVnRs"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("হ্যালো 😄 আমি alive!")
+    await update.message.reply_text("হ্যালো 😄 আমি alive! (Bot by Saikat)"
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
