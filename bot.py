e1559f1721532997236e9fb20b49006c629b5be5
sinharzs2k26 <sinharzs2k26@gmail.com>
2026-01-17 07:28:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
-from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-app = FastAPI()
-
 TOKEN = "8221663079:AAHR01BGwId6N4aL8HYu7yUwBK4n6eJ8Fng"
 
-# Bot application
-telegram_app = ApplicationBuilder().token(TOKEN).build()
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("হ্যালো! Webhook কাজ করছে!")
+    await update.message.reply_text("হ্যালো 😄 আমি alive!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
-telegram_app.add_handler(CommandHandler("start", start))
-telegram_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app = ApplicationBuilder().token(TOKEN).build()
 
-# Webhook endpoint
-@app.post("/webhook")
-async def webhook(request: Request):
-    data = await request.json()
-    update = Update.de_json(data, telegram_app.bot)
-    await telegram_app.process_update(update)
-    return {"ok": True}
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("Bot is running...")
+app.run_polling()
